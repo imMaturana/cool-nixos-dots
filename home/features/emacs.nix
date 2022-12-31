fdd9a80ff1e8313133d5817b9e9484b93942b129
imMaturana <git@maturanei.anonaddy.com>
2022-12-31 19:00:12 -0400

    home/emacs: add configuration

--- home/features/emacs.nix
+++ home/features/emacs.nix
@@ -3,6 +3,7 @@
   config,
   ...
 }:
+
 {
   home.packages = [pkgs.emacs-all-the-icons-fonts];
 
@@ -11,13 +12,109 @@
     package = pkgs.emacsPgtk;
 
     extraPackages = epkgs: with epkgs; [
+      # ui
+      doom-modeline
+      base16-theme
+      all-the-icons
+      dashboard
+      vertico
+
+      # programming
+      nix-mode
+      go-mode
+      zig-mode
+      lsp-pyright
+
+      # lsp
+      orderless
+      corfu
+      eglot
+
+      # tools
+      direnv
+      magit
       vterm
     ];
+
+    extraConfig = ''
+      (progn ; ui
+        ;; disable some stuff
+        (menu-bar-mode -1)
+        (scroll-bar-mode -1)
+        (tool-bar-mode -1)
+
+        ;; enable truncate lines
+        (setq truncate-lines t)
+
+        ;; font
+        (set-face-attribute 'default nil :font "${config.fontProfiles.monospace.family}")
+        (add-to-list 'default-frame-alist '(font . "${config.fontProfiles.monospace.family}-10"))
+
+        ;; line numbers
+        (global-display-line-numbers-mode)
+
+        ;; tab width
+        (setq-default tab-width 4)
+
+        ;; theme
+        (require 'base16-theme)
+        (load-theme 'base16-${config.colorscheme.slug} t)
+
+        ;; modeline
+        (require 'doom-modeline)
+        (doom-modeline-mode)
+
+        ;; dashboard
+        (require 'dashboard)
+        (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
+        (dashboard-setup-startup-hook)
+
+        ;; vertico
+        (require 'vertico)
+        (vertico-mode))
+
+      (progn ; keybindings
+        ;; IBuffer
+        (global-set-key (kbd "C-x C-b") 'ibuffer))
+
+      (progn ; programming
+        ;; python
+        (require 'lsp-pyright)
+        (setq lsp-pyright-auto-import-completions t
+              lsp-pyright-use-library-code-for-types t)
+        (add-hook 'python-mode-hook (lambda () (lsp)))
+
+        ;; go
+        (require 'go-mode)
+        (add-hook 'go-mode-hook 'eglot-ensure)
+
+        ;; zig
+        (require 'zig-mode)
+        (add-hook 'zig-mode-hook 'eglot-ensure)
+
+        ;; direnv
+        (require 'direnv)
+        (direnv-mode)
+
+        ;; orderless
+        (require 'orderless)
+        (setq completion-styles '(orderless flex)
+              cempletion-category-overrides '(eglot (styles . (orderless-flex))))
+
+        ;; corfu
+        (require 'corfu)
+        (setq corfu-auto t)
+        (global-corfu-mode)
+
+        ;; eglot
+        (require 'eglot)
+        (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1))))
+    '';
   };
 
   services.emacs = {
     enable = true;
-    package = config.programs.emacs.package;
+    package = config.programs.emacs.finalPackage;
   };
 
   programs.git.ignores = [
