e5a9f6e1c0a27b22905a77fe3a81912a7690a310
imMaturana <git@maturana.simplelogin.com>
2023-12-19 17:59:57 -0300

    home/shared/git: change user email

--- home/shared/git.nix
+++ home/shared/git.nix
@@ -2,7 +2,13 @@
   programs.git = {
     enable = true;
     userName = "imMaturana";
-    userEmail = "maturana.dev@protonmail.com";
+    userEmail = "git@maturana.simplelogin.com";
+
+    signing = {
+      key = "CBB817BF7AB06ADBB418646CEC57406107E88C18";
+      signByDefault = true;
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
