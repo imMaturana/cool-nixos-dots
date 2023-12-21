05b46f4fe52fbc1514901edfc585ab8ee368b283
imMaturana <git@maturana.simplelogin.com>
2023-12-21 17:43:04 -0300

    home/s/packages: add `age` and `minisign`
    Sort lines too

--- home/shared/packages.nix
+++ home/shared/packages.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    age
     amfora
+    cryptsetup
+    ffmpeg
+    imagemagick
     keepassxc
+    minisign
+    ripgrep
+    unzip
     yt-dlp
     zip
-    unzip
-    cryptsetup
-    imagemagick
-    ffmpeg
-    ripgrep
   ];
 }
