5a31fd1b8a288f3b6fd78ceba61ebc0c9aeb1615
imMaturana <git@maturana.simplelogin.com>
2023-12-29 18:39:47 -0300

    home/git: use ssh to sign commits

--- home/shared/git.nix
+++ home/shared/git.nix
@@ -2,15 +2,20 @@
   programs.git = {
     enable = true;
     userName = "imMaturana";
-    userEmail = "git@maturana.simplelogin.com";
+    userEmail = "git.frighten632@simplelogin.fr";
 
     signing = {
-      key = "CBB817BF7AB06ADBB418646CEC57406107E88C18";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIApciVrwNsMaaPNIPn9CMoHfKSx1Gzq/QY8Ri4ResOAW";
       signByDefault = true;
     };
 
     extraConfig = {
       init.defaultBranch = "main";
+      gpg.format = "ssh";
     };
+  };
+
+  services.ssh-agent = {
+    enable = true;
   };
 }
